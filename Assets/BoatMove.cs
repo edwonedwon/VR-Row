3972803df19151ed9f6f99db9145c3bd65a494de
Steven Edwon <edwon@me.com>
2014-09-07 12:28:00 +0900

    test boat movement implemented in lake scene

---
+++ Assets/BoatMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMove : MonoBehaviour {
+
+	public float speed;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	void FixedUpdate ()
+	{
+		transform.position += new Vector3(0,0,-speed);
+	}
+}

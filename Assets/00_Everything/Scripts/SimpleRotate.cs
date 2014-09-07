85b60161b0362f24aac2fa48a88df9f65f35c064
Andrew Palmer <andyp.123@gmail.com>
2014-09-08 02:18:31 +0900

    Added spaceboat and a crap test scene

---
+++ Assets/00_Everything/Scripts/SimpleRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotate : MonoBehaviour {
+
+	public float degrees_per_second = 45.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * degrees_per_second);	
+	}
+}

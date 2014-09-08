4596ed2dccac936016244000feb434aac8d65802
Steven Edwon <edwon@me.com>
2014-09-08 09:00:27 +0900

    prepare all levels for builds

--- Assets/00_Everything/Scripts/SimpleRotate.cs
+++ Assets/00_Everything/Scripts/SimpleRotate.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class SimpleRotate : MonoBehaviour {
 
-	public float degrees_per_second = 45.0f;
-	
+	public float rotate_scale = 100.0f;
+	Vector3 lastPosition = Vector3.zero;
+
+	void Start() {
+		lastPosition = transform.position;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.forward * Time.deltaTime * degrees_per_second);	
+		Vector3 move = transform.position - lastPosition;
+		float movescale = move.magnitude * rotate_scale;
+		transform.Rotate(Vector3.forward * Time.deltaTime * movescale);	
+		lastPosition = transform.position;
 	}
 }
